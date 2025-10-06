a7375c15cdfa28c6ec2944f58d3c5d8cec3cc31b
riplegend011 <riplegend444@gmail.com>
2025-10-06 22:22:35 +0600

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ dotenv.config();
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 });
+const port = process.env.PORT || 4000 
 
 client.once("ready", () => {
   console.log(`✅ Logged in as ${client.user.tag}`);
